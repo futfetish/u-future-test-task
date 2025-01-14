7228c91a572a1a2d1b5764e7b282d90f7c80260f
futfetish <gr1zzly.medvegonok@gmail.com>
2025-01-14 18:55:34 +0300

    delete task

--- src/components/task/taskItem.tsx
+++ src/components/task/taskItem.tsx
@@ -3,38 +3,54 @@ import { TaskI } from "../../types/task";
 import Styles from "@/styles/task/taskItem.module.scss";
 import clsx from "clsx";
 
-export const TaskItem: FC<{ task: TaskI }> = React.memo(
-  ({ task }) => {
-    return (
-      <div className={Styles.item}>
-        <div className={Styles.top}>
-          <h3>{task.title}</h3>
-          <div className={Styles.thing}>
-            <p>{task.priority ? <>{task.priority}</> : <> none </>}</p>
-            <p>priority</p>
-          </div>
+export const TaskItem: FC<{ task: TaskI; deleteF: (id: number) => void }> =
+  React.memo(
+    ({ task, deleteF }) => {
+      const deleteTask = () => {
+        if (
+          window.confirm(
+            `Вы уверены, что хотите удалить задачу "${task.title}"?`
+          )
+        ) {
+          deleteF(task.id);
+        }
+      };
 
-          <div className={Styles.thing}>
-            <p> completed: </p>
-            <div
-              className={clsx(
-                Styles.check,
-                task.completed && Styles.check_active
-              )}
-            ></div>
+      return (
+        <div className={Styles.item}>
+          <div className={Styles.top}>
+            <div className={Styles.info}>
+              <h3>{task.title}</h3>
+              <div className={Styles.thing}>
+                <p>{task.priority ? <>{task.priority}</> : <> none </>}</p>
+                <p>priority</p>
+              </div>
+
+              <div className={Styles.thing}>
+                <p> completed: </p>
+                <div
+                  className={clsx(
+                    Styles.check,
+                    task.completed && Styles.check_active
+                  )}
+                ></div>
+              </div>
+            </div>
+            <a href="#" onClick={() => deleteTask()} className={Styles.delete}>
+              x
+            </a>
           </div>
+          <div className="stick"></div>
+          <div className={Styles.description}> {task.description} </div>
         </div>
-        <div className="stick"></div>
-        <div className={Styles.description}> {task.description} </div>
-      </div>
-    );
-  },
-  (prev, next) => {
-    return (
-      prev.task.id === next.task.id &&
-      prev.task.priority === next.task.priority &&
-      prev.task.completed === next.task.completed &&
-      prev.task.description === next.task.description
-    );
-  }
-);
+      );
+    },
+    (prev, next) => {
+      return (
+        prev.task.id === next.task.id &&
+        prev.task.priority === next.task.priority &&
+        prev.task.completed === next.task.completed &&
+        prev.task.description === next.task.description
+      );
+    }
+  );
--- src/components/task/taskList.tsx
+++ src/components/task/taskList.tsx
@@ -1,17 +1,25 @@
-import {  FC } from "react";
+import { FC } from "react";
 import { TaskI } from "../../types/task";
 import { TaskItem } from "./taskItem";
-import Styles from '@/styles/task/taskList.module.scss'
+import Styles from "@/styles/task/taskList.module.scss";
+import { useDeleteTaskMutation } from "../../store/api/taskApi";
 
-export const TaskList: FC<{tasks: TaskI[]}> = ({tasks}) => {
-
+export const TaskList: FC<{ tasks: TaskI[] }> = ({ tasks }) => {
+  const [deleteTask] = useDeleteTaskMutation();
+  const handleDelete = async (id: number) => {
+    try {
+      await deleteTask(id).unwrap(); // Отправляем запрос на удаление
+      window.location.reload();
+    } catch {
+      alert("Произошла ошибка при удалении задачи.");
+    }
+  };
 
   return (
-    <div className={Styles.list} >
+    <div className={Styles.list}>
       {tasks?.map((task) => (
-        <TaskItem task={task}  key={task.id} />
+        <TaskItem deleteF={handleDelete} task={task} key={task.id} />
       ))}
     </div>
   );
 };
-
